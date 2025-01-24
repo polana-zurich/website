c17386a8ec8df0e9aa47f9285c83af962dd63927
Jakub Potempa <jpotempa@student.ethz.ch>
2025-01-24 19:16:17 +0100

    add favicon, photos displayed by default

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,5 +1,9 @@
 var coll = document.getElementsByClassName("collapsible");
 
+coll[0].classList.toggle("active");
+var content = coll[0].nextElementSibling;
+content.style.maxHeight = content.scrollHeight + "px";
+
 for (i = 0; i < coll.length; i++) {
 	coll[i].addEventListener("click", function() {
 		this.classList.toggle("active");
