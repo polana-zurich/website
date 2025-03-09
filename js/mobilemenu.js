de46834fb103ddeeaff46b5cf130c5749ebe14cf
Jakub Potempa <jpotempa@student.ethz.ch>
2025-03-09 13:42:31 +0100

    Refresh index, center mobile menu closing button

--- js/mobilemenu.js
+++ js/mobilemenu.js
@@ -1,8 +1,10 @@
 function showMenu(){
 	var nav = document.getElementsByClassName("mobile-menu-item");
-	for(var i=0; i<nav.length; i++){		
+	for(var i=0; i<nav.length; i++){
 		nav[i].style.display = "block";
 	}
+	var closeButton = document.getElementsByClassName("mobile-button-close");
+	closeButton[0].style.display = "grid";
 	setTimeout(function() {
 		for(var i=0; i<nav.length; i++){
 			nav[i].style.right = "0px";
